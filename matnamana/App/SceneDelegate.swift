c0c5ab68da877f095ac5a9b15998a120c17736c7
LeeJinKyu <jade981220@gmail.com>
2024-09-10 03:38:58 +0900

    fix: 시작시 로그인 가능하게, 평판조회 가능하게 수정
    윤홍님이랑 시뮬 영상 찍으면서한거 커밋

--- matnamana/App/SceneDelegate.swift
+++ matnamana/App/SceneDelegate.swift
@@ -14,26 +14,37 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    
+
     guard let windowScene = (scene as? UIWindowScene) else { return }
     let window = UIWindow(windowScene: windowScene)
 
+<<<<<<< Updated upstream
     let isLoggedIn = UserDefaults.standard.bool(forKey: "isLoggedIn")
     
+=======
+   let isLoggedIn = false//UserDefaults.standard.bool(forKey: "isLoggedIn")
+
+>>>>>>> Stashed changes
     if isLoggedIn {
       window.rootViewController = TabBarController()
     } else {
       window.rootViewController = LoginController()
     }
+<<<<<<< Updated upstream
     
     UserDefaults.standard.set("UzhyIQp6J2a0JxORcwabPec4qUf1", forKey: "loggedInUserId")
     
+=======
+   // window.rootViewController = TabBarController()
+//    UserDefaults.standard.set("UzhyIQp6J2a0JxORcwabPec4qUf1", forKey: "loggedInUserId")
+
+>>>>>>> Stashed changes
     DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
       window.makeKeyAndVisible()
     }
     self.window = window
   }
-  
+
   func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
     if let url = URLContexts.first?.url {
       if (AuthApi.isKakaoTalkLoginUrl(url)) {
