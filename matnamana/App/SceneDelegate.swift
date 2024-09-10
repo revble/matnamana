3eeb3e66f484fbd886f415e63687195406a32b65
LeeJinKyu <jade981220@gmail.com>
2024-09-10 15:22:37 +0900

    Fix: 저장시 친구 목록 삭제 버그 수정

--- matnamana/App/SceneDelegate.swift
+++ matnamana/App/SceneDelegate.swift
@@ -4,6 +4,55 @@
 //
 //  Created by 김윤홍 on 8/23/24.
 //
+//
+//import UIKit
+//
+//import KakaoSDKAuth
+//
+//class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+//
+//  var window: UIWindow?
+//
+//  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//
+//    guard let windowScene = (scene as? UIWindowScene) else { return }
+//    let window = UIWindow(windowScene: windowScene)
+//
+//<<<<<<< Updated upstream
+//    let isLoggedIn = UserDefaults.standard.bool(forKey: "isLoggedIn")
+//    
+//=======
+//   let isLoggedIn = false//UserDefaults.standard.bool(forKey: "isLoggedIn")
+//
+//>>>>>>> Stashed changes
+//    if isLoggedIn {
+//      window.rootViewController = TabBarController()
+//    } else {
+//      window.rootViewController = LoginController()
+//    }
+//<<<<<<< Updated upstream
+//    
+//    UserDefaults.standard.set("UzhyIQp6J2a0JxORcwabPec4qUf1", forKey: "loggedInUserId")
+//    
+//=======
+//   // window.rootViewController = TabBarController()
+////    UserDefaults.standard.set("UzhyIQp6J2a0JxORcwabPec4qUf1", forKey: "loggedInUserId")
+//
+//>>>>>>> Stashed changes
+//    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+//      window.makeKeyAndVisible()
+//    }
+//    self.window = window
+//  }
+//
+//  func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+//    if let url = URLContexts.first?.url {
+//      if (AuthApi.isKakaoTalkLoginUrl(url)) {
+//        _ = AuthController.handleOpenUrl(url: url)
+//      }
+//    }
+//  }
+//}
 
 import UIKit
 
@@ -18,27 +67,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     let window = UIWindow(windowScene: windowScene)
 
-<<<<<<< Updated upstream
     let isLoggedIn = UserDefaults.standard.bool(forKey: "isLoggedIn")
-    
-=======
-   let isLoggedIn = false//UserDefaults.standard.bool(forKey: "isLoggedIn")
 
->>>>>>> Stashed changes
     if isLoggedIn {
       window.rootViewController = TabBarController()
     } else {
       window.rootViewController = LoginController()
     }
-<<<<<<< Updated upstream
-    
-    UserDefaults.standard.set("UzhyIQp6J2a0JxORcwabPec4qUf1", forKey: "loggedInUserId")
-    
-=======
-   // window.rootViewController = TabBarController()
-//    UserDefaults.standard.set("UzhyIQp6J2a0JxORcwabPec4qUf1", forKey: "loggedInUserId")
 
->>>>>>> Stashed changes
+    UserDefaults.standard.set("WIz5GY8obiXPngauYYhI7OcCyLr1", forKey: "loggedInUserId")
+
     DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
       window.makeKeyAndVisible()
     }
--- matnamana/ViewModel/Profile/ProfileEditViewModel.swift
+++ matnamana/ViewModel/Profile/ProfileEditViewModel.swift
@@ -10,9 +10,11 @@ import FirebaseFirestore
 import RxCocoa
 import RxSwift
 
+
+
 final class ProfileEditViewModel: ViewModelType {
 
-  private let db = FirebaseManager.shared.db
+  //private let db = FirebaseManager.shared.db
 
   struct Input {
     let saveTap: Observable<Void>
@@ -29,7 +31,7 @@ final class ProfileEditViewModel: ViewModelType {
   }
 
   // Firebase에서 사용자 정보를 불러오는 메서드 추가
-  private func fetchProfileData() -> Observable<User.Info> {
+  private func fetchProfileData() -> Observable<User> {
     return Observable.create { observer in
       guard let loggedInUserId = UserDefaults.standard.string(forKey: "loggedInUserId") else {
         observer.onError(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "User ID not found"]))
@@ -40,10 +42,10 @@ final class ProfileEditViewModel: ViewModelType {
         if let error = error {
           observer.onError(error)
         } else if let user = user {
-          observer.onNext(user.info)
+          observer.onNext(user)
           observer.onCompleted()
         } else {
-          observer.onNext(User.Info.empty)
+          observer.onNext(User(info: .empty, preset: [], friendList: [], userId: loggedInUserId))
           observer.onCompleted()
         }
       }
@@ -58,34 +60,34 @@ final class ProfileEditViewModel: ViewModelType {
       .flatMap { [weak self] (name, nickName, shortDescription, userInfo, profileImageUrl) -> Observable<Bool> in
         guard let self = self else { return Observable.just(false) }
 
-        let info = User.Info(
-          career: userInfo["직업"] ?? "",
-          education: userInfo["최종학력"] ?? "",
-          email: userInfo["이메일"] ?? "",
-          location: userInfo["거주지"] ?? "",
-          name: name,
-          phoneNumber: userInfo["휴대번호"] ?? "",
-          shortDescription: shortDescription,
-          profileImage: profileImageUrl,
-          nickName: nickName,
-          birth: userInfo["생일"] ?? "",
-          university: userInfo["대학교"] ?? "",
-          companyName:userInfo["회사"] ?? ""
+        return self.fetchProfileData()
+          .flatMap { existingUser -> Observable<Bool> in
+            // 기존의 User 객체를 받아와 필요한 필드만 업데이트
+            let updatedInfo = User.Info(
+              career: userInfo["직업"] ?? existingUser.info.career,
+              education: userInfo["최종학력"] ?? existingUser.info.education,
+              email: userInfo["이메일"] ?? existingUser.info.email,
+              location: userInfo["거주지"] ?? existingUser.info.location,
+              name: name,
+              phoneNumber: userInfo["휴대번호"] ?? existingUser.info.phoneNumber,
+              shortDescription: shortDescription,
+              profileImage: profileImageUrl,
+              nickName: nickName,
+              birth: userInfo["생일"] ?? existingUser.info.birth,
+              university: userInfo["대학교"] ?? existingUser.info.university,
+              companyName: userInfo["회사"] ?? existingUser.info.companyName
+            )
 
-        )
+            // 기존 preset과 friendList를 유지
+            let updatedUser = User(info: updatedInfo, preset: existingUser.preset, friendList: existingUser.friendList, userId: existingUser.userId)
 
-        guard let loggedInUserId = UserDefaults.standard.string(forKey: "loggedInUserId") else {
-          return Observable.just(false)
-        }
-
-        let user = User(info: info, preset: [], friendList: [], userId: loggedInUserId)
-
-        return self.saveUserData(user: user)
+            return self.saveUserData(user: updatedUser)
+          }
       }
       .asDriver(onErrorJustReturn: false)
 
     // Firebase에서 사용자 정보를 불러오는 Observable
-    let profileData = fetchProfileData()
+    let profileData = fetchProfileData().map { $0.info }
 
     return Output(saveResult: saveResult, currentUserInfo: profileData)
   }
@@ -93,10 +95,97 @@ final class ProfileEditViewModel: ViewModelType {
   // Firebase에 사용자 데이터를 저장하는 메서드
   private func saveUserData(user: User) -> Observable<Bool> {
     return Observable.create { observer in
-//      FirebaseManager.shared.addUser(user: user)
+      FirebaseManager.shared.addData(to: .user, data: user, documentId: user.userId)
       observer.onNext(true)
       observer.onCompleted()
       return Disposables.create()
     }
   }
 }
+
+//  struct Input {
+//    let saveTap: Observable<Void>
+//    let nameText: Observable<String>
+//    let nicknameText: Observable<String>
+//    let shortDescriptionText: Observable<String>
+//    let userInfoTexts: Observable<[String: String]>
+//    let profileImageUrl: Observable<String>  // 이미지 URL을 받을 Observable 추가
+//  }
+//
+//  struct Output {
+//    let saveResult: Driver<Bool>
+//    let currentUserInfo: Observable<User.Info>  // Firebase에서 가져온 사용자 정보를 반환하는 Observable 추가
+//  }
+//
+//  // Firebase에서 사용자 정보를 불러오는 메서드 추가
+//  private func fetchProfileData() -> Observable<User.Info> {
+//    return Observable.create { observer in
+//      guard let loggedInUserId = UserDefaults.standard.string(forKey: "loggedInUserId") else {
+//        observer.onError(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "User ID not found"]))
+//        return Disposables.create()
+//      }
+//
+//      FirebaseManager.shared.readUser(documentId: loggedInUserId) { user, error in
+//        if let error = error {
+//          observer.onError(error)
+//        } else if let user = user {
+//          observer.onNext(user.info)
+//          observer.onCompleted()
+//        } else {
+//          observer.onNext(User.Info.empty)
+//          observer.onCompleted()
+//        }
+//      }
+//      return Disposables.create()
+//    }
+//  }
+//
+//  // 입력을 받아 사용자 정보 저장 및 업데이트하는 메서드
+//  func transform(input: Input) -> Output {
+//    let saveResult = input.saveTap
+//      .withLatestFrom(Observable.combineLatest(input.nameText, input.nicknameText, input.shortDescriptionText, input.userInfoTexts, input.profileImageUrl))  // 이미지 URL 포함
+//      .flatMap { [weak self] (name, nickName, shortDescription, userInfo, profileImageUrl) -> Observable<Bool> in
+//        guard let self = self else { return Observable.just(false) }
+//
+//        let info = User.Info(
+//          career: userInfo["직업"] ?? "",
+//          education: userInfo["최종학력"] ?? "",
+//          email: userInfo["이메일"] ?? "",
+//          location: userInfo["거주지"] ?? "",
+//          name: name,
+//          phoneNumber: userInfo["휴대번호"] ?? "",
+//          shortDescription: shortDescription,
+//          profileImage: profileImageUrl,
+//          nickName: nickName,
+//          birth: userInfo["생일"] ?? "",
+//          university: userInfo["대학교"] ?? "",
+//          companyName:userInfo["회사"] ?? ""
+//
+//        )
+//
+//        guard let loggedInUserId = UserDefaults.standard.string(forKey: "loggedInUserId") else {
+//          return Observable.just(false)
+//        }
+//
+//        let user = User(info: info, preset: [], friendList: [], userId: loggedInUserId)
+//
+//        return self.saveUserData(user: user)
+//      }
+//      .asDriver(onErrorJustReturn: false)
+//
+//    // Firebase에서 사용자 정보를 불러오는 Observable
+//    let profileData = fetchProfileData()
+//
+//    return Output(saveResult: saveResult, currentUserInfo: profileData)
+//  }
+//
+//  // Firebase에 사용자 데이터를 저장하는 메서드
+//  private func saveUserData(user: User) -> Observable<Bool> {
+//    return Observable.create { observer in
+////      FirebaseManager.shared.addUser(user: user)
+//      observer.onNext(true)
+//      observer.onCompleted()
+//      return Disposables.create()
+//    }
+//  }
+//}
