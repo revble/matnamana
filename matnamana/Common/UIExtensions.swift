af02ff209e2636d830e38f39bf3b16e4a1e3ce9c
dbsghdz1 <dbsghdz1@gmail.com>
2024-09-14 17:36:09 +0900

    Feat: 글꼴 추가

--- matnamana/Common/UIExtensions.swift
+++ matnamana/Common/UIExtensions.swift
@@ -13,6 +13,7 @@ extension UIColor {
   static let manaMint = UIColor(red: 199/255, green: 220/255, blue: 167/255, alpha: 1)
   static let manaGreen = UIColor(red: 137/255, green: 185/255, blue: 173/255, alpha: 1)
   static let manaMainColor = UIColor(red: 0/255, green: 168/255, blue: 226/255, alpha: 1)
+  static let manatextColor = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
 }
 extension UIViewController {
   func transitionToViewController(_ viewController: UIViewController) {
@@ -48,5 +49,47 @@ extension UIView {
     self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
     self.layer.shadowOffset = CGSize(width: 2, height: 2)
     self.layer.shadowRadius = 2
+  }
+}
+
+extension UILabel {
+  func screenTitle(fontSize: CGFloat = 34) {
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+  }
+  
+  func title1(fontSize: CGFloat = 28) {
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+  }
+  
+  func title2(fontSize: CGFloat = 22) {
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+  }
+  
+  func title3(fontSize: CGFloat = 20) {
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .semibold)
+  }
+  
+  func headLine(fontSize: CGFloat = 17) {
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .semibold)
+  }
+  
+  func bonmoon(fontSize: CGFloat = 17) {
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .regular)
+  }
+  
+  func callOut(fontSize: CGFloat = 16) {
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .semibold)
+  }
+  
+  func subHeadLine(fontSize: CGFloat = 15) {
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .regular)
+  }
+  
+  func footNote(fontSize: CGFloat = 13) {
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .regular)
+  }
+  
+  func caption(fontSize: CGFloat = 12) {
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .regular)
   }
 }
--- matnamana/View/MyQuestion/BannerCell.swift
+++ matnamana/View/MyQuestion/BannerCell.swift
@@ -11,28 +11,33 @@ import SnapKit
 import Then
 
 final class BannerCell: UICollectionViewCell {
+  
   private let bannerImageView = UIImageView().then {
     $0.contentMode = .scaleAspectFill
     $0.clipsToBounds = true
+    $0.image = UIImage(named: "mainBannerImage")
+    $0.layer.cornerRadius = 16
   }
   
   override init(frame: CGRect) {
     super.init(frame: frame)
-    setupUI()
+    configureUI()
+    setConstraints()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  private func setupUI() {
+  private func configureUI() {
     contentView.addSubview(bannerImageView)
-    bannerImageView.snp.makeConstraints { make in
-      make.edges.equalToSuperview()
-    }
   }
   
-  func configure(with image: UIImage) {
-    bannerImageView.image = image
+  private func setConstraints() {
+    
+    bannerImageView.snp.makeConstraints {
+      $0.horizontalEdges.equalToSuperview().inset(20)
+      $0.height.equalTo(120)
+    }
   }
 }
